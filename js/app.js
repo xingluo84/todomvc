d4fd40308acc35e2d14c1c4d8e90f3f136337a22
pipixia <pipixia@ppx.com>
2017-06-27 10:32:27 +0800

    02-展示任务列表

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-(function (window) {
+(function(angular) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	angular
+		.module('todoApp', [])
+		.controller('TodoController', ['$scope', TodoController]);
+	
+	
+	// 控制器函数
+	function TodoController($scope) {
+		var vm = $scope;
 
-})(window);
+		// 代码写在此处！
+
+		// 1 展示任务列表
+		// 抽象数据结构
+		var todoList = [
+			{ id: 1, name: '抽烟', isCompleted: false },
+			{ id: 2, name: '喝酒', isCompleted: false },
+			{ id: 3, name: '烫头', isCompleted: true },
+		];
+
+		vm.todoList = todoList;
+	}
+
+})(angular);
