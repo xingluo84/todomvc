20313b4d2fbf28c8b5ac97a09ceecec728caf2f5
pipixia <pipixia@ppx.com>
2017-06-27 10:52:27 +0800

    03-添加任务

--- js/app.js
+++ js/app.js
@@ -2,25 +2,54 @@
 	'use strict';
 
 	angular
+		// 创建模块
 		.module('todoApp', [])
+		// 创建控制器
 		.controller('TodoController', ['$scope', TodoController]);
 	
 	
 	// 控制器函数
 	function TodoController($scope) {
 		var vm = $scope;
-
 		// 代码写在此处！
 
 		// 1 展示任务列表
-		// 抽象数据结构
+		// 根据视图，抽象数据结构
+		// 	id 用来确定唯一一条数据
+		//  name 表示当前任务名称
+		//  isCompleted 表示当前任务是否完成
 		var todoList = [
 			{ id: 1, name: '抽烟', isCompleted: false },
 			{ id: 2, name: '喝酒', isCompleted: false },
 			{ id: 3, name: '烫头', isCompleted: true },
 		];
-
+		// 给view提供数据
 		vm.todoList = todoList;
+
+		// 2 添加任务
+
+		// 任务名称
+		vm.taskName = '';
+		vm.add = function() {
+			// 判断任务名称是否为空
+			if(vm.taskName.trim() === '') {
+				return;
+			}
+
+			// 处理id
+			// 思路：获取到数组最后一项的id，再加1就是当前要添加的id
+			var id;
+			if( todoList.length === 0 ) {
+				// 如果数组中没有数据，那么，id就是1
+				id = 1;
+			} else {
+				id = todoList[todoList.length - 1].id + 1;
+			}
+
+			todoList.push({ id: id, name: vm.taskName, isCompleted: false });
+			// 清空文本框（清空 taskName 属性的值）
+			vm.taskName = '';
+		};
 	}
 
 })(angular);
